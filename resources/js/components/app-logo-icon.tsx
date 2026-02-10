65a82819357f80274afa18114c51b9ac384c4ed6
Mike Beasley <mbeasley4@gmail.com>
2026-02-10 16:40:55 -0500

    update image

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,7 +1,7 @@
 export default function AppLogoIcon() {
     return (
           <img
-            src="/images/logos/f30-cohort.png"
+            src="/images/logos/f30-cycle.png"
             alt="Fit30 Community"
             className="mb-6 h-12 w-auto"
         />
